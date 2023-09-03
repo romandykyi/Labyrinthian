9a80e8e3ab4638d7f384ad7bf682a084d80ad2ce
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-03 12:11:20 +0200

    Add a constructor for 'SvgStop'

--- Labyrinthian/Svg/Elements/Definitions/SvgStop.cs
+++ Labyrinthian/Svg/Elements/Definitions/SvgStop.cs
@@ -9,5 +9,12 @@
         public SvgColor? StopColor { get; set; }
         [SvgProperty("stop-opacity")]
         public SvgUnit? StopOpacity { get; set; }
+
+        public SvgStop(SvgUnit? offset = null, SvgColor? stopColor = null, SvgUnit? stopOpacity = null)
+        {
+            Offset = offset;
+            StopColor = stopColor;
+            StopOpacity = stopOpacity;
+        }
     }
 }
