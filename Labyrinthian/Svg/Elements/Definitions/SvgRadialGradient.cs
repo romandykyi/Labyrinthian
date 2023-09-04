40a6280ef904f1cdbed15b6041692086e2b5c3ff
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-04 18:57:39 +0200

    Make 'SvgRadialGradient' properties optional

--- Labyrinthian/Svg/Elements/Definitions/SvgRadialGradient.cs
+++ Labyrinthian/Svg/Elements/Definitions/SvgRadialGradient.cs
@@ -4,16 +4,16 @@
     public sealed class SvgRadialGradient : SvgGradient
     {
         [SvgProperty("cx")]
-        public SvgLength Cx { get; set; }
+        public SvgLength? Cx { get; set; }
         [SvgProperty("cy")]
-        public SvgLength Cy { get; set; }
+        public SvgLength? Cy { get; set; }
         [SvgProperty("r")]
-        public SvgLength R { get; set; }
+        public SvgLength? R { get; set; }
         [SvgProperty("fr")]
-        public SvgLength Fr { get; set; }
+        public SvgLength? Fr { get; set; }
         [SvgProperty("fx")]
-        public SvgLength Fx { get; set; }
+        public SvgLength? Fx { get; set; }
         [SvgProperty("fy")]
-        public SvgLength Fy { get; set; }
+        public SvgLength? Fy { get; set; }
     }
 }
