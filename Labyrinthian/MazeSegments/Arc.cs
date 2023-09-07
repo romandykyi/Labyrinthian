ffbb66985a0507eeaa272374efa573ae6e621e89
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-07 15:30:41 +0200

    Add 'Clockwise' property for 'Arc'

--- Labyrinthian/MazeSegments/Arc.cs
+++ Labyrinthian/MazeSegments/Arc.cs
@@ -4,6 +4,7 @@ namespace Labyrinthian
 {
     public sealed class Arc : PathSegment
     {
+        private readonly bool _clockwise = true;
         private readonly float _rX, _rY;
         private readonly Vector2 _start, _end;
 
@@ -12,24 +13,27 @@ namespace Labyrinthian
 
         public float Rx => _rX;
         public float Ry => _rY;
+        public bool Clockwise => _clockwise;
 
-        public Arc(Vector2 start, Vector2 end, float rX, float rY)
+        public Arc(Vector2 start, Vector2 end, float rX, float rY, bool clockwise = true)
         {
             _start = start;
             _end = end;
             _rX = rX;
             _rY = rY;
+            _clockwise = clockwise;
         }
 
-        public Arc(Vector2 start, Vector2 end, float r) : this(start, end, r, r)
+        public Arc(Vector2 start, Vector2 end, float r, bool clockwise = true) : 
+            this(start, end, r, r, clockwise)
         { }
 
-        public Arc(float x1, float y1, float x2, float y2, float rX, float rY) :
-            this(new Vector2(x1, y1), new Vector2(x2, y2), rX, rY)
+        public Arc(float x1, float y1, float x2, float y2, float rX, float rY, bool clockwise = true) :
+            this(new Vector2(x1, y1), new Vector2(x2, y2), rX, rY, clockwise)
         { }
 
-        public Arc(float x1, float y1, float x2, float y2, float r) :
-            this(x1, y1, x2, y2, r, r)
+        public Arc(float x1, float y1, float x2, float y2, float r, bool clockwise = true) :
+            this(x1, y1, x2, y2, r, r, clockwise)
         { }
 
         public override string MoveToEndSvg(float cellSize, float offset)
@@ -38,7 +42,7 @@ namespace Labyrinthian
             float rY = TransformSize(_rY, cellSize);
             Vector2 end = TransformPoint(EndPoint, cellSize, offset);
 
-            return $"A{rX.ToInvariantString()},{rY.ToInvariantString()} 0 0 1 {end.X.ToInvariantString()} {end.Y.ToInvariantString()} ";
+            return $"A{rX.ToInvariantString()},{rY.ToInvariantString()} 0 0 {(_clockwise ? '1' : '0')} {end.X.ToInvariantString()} {end.Y.ToInvariantString()} ";
         }
     }
 }
