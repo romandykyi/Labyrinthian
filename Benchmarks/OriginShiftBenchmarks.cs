b536e76ee75d3cd5aa79b51bd178f6968b3cb5fd
romandykyi <roman.dykyi.8210@gmail.com>
2024-07-06 20:32:36 +0200

    Add benchmarks and origin shift analysis

---
+++ Benchmarks/OriginShiftBenchmarks.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Labyrinthian;
+
+namespace Benchmarks;
+
+[ShortRunJob(RuntimeMoniker.Net80)]
+public class OriginShiftBenchmarks
+{
+	private const int Seed = 123;
+
+	private Maze _maze = null!;
+
+	[Params(20)]
+	public int MazeSize { get; set; }
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		_maze = new OrthogonalMaze(MazeSize, MazeSize);
+	}
+
+	[Benchmark]
+	public Maze AldousBroder() => new AldousBroderGeneration(_maze, seed: Seed).Generate();
+	[Benchmark]
+	public Maze DFSGeneration() => new DFSGeneration(_maze, seed: Seed).Generate();
+	[Benchmark]
+	public Maze Kruskal() => new KruskalGeneration(_maze, seed: Seed).Generate();
+	[Benchmark]
+	public Maze OriginShift() => new OriginShiftGeneration(_maze, seed: Seed).Generate();
+	[Benchmark]
+	public Maze Prim() => new PrimGeneration(_maze, seed: Seed).Generate();
+	[Benchmark]
+	public Maze Wilson() => new WilsonGeneration(_maze, seed: Seed).Generate();
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,92 @@
+﻿using Labyrinthian;
+
+//BenchmarkRunner.Run(typeof(OriginShiftBenchmarks));
+
+const int seed = 3403;
+const int tests = 500;
+const int deltaFactorsNumber = 21;
+
+Random rnd = new(seed);
+
+Func<Maze>[] mazeTypes =
+[
+	() => new OrthogonalMaze(5, 5),
+	() => new OrthogonalMaze(10, 10),
+	() => new OrthogonalMaze(20, 20),
+	() => new OrthogonalMaze(30, 30),
+	() => new OrthogonalMaze(50, 50),
+];
+
+float[] deltaFactors = Enumerable.Range(0, deltaFactorsNumber)
+	.Select(n => (float)n / (deltaFactorsNumber - 1))
+	.ToArray();
+
+int TestGenerator(OriginShiftGeneration generator)
+{
+	int visitedCells = 0;
+
+	void OnMarkChanged(CellMark mark)
+	{
+		if (mark.Value) visitedCells++;
+	}
+
+	generator.VisitedCells.MarkChanged += OnMarkChanged;
+
+	int iterations = 0;
+	var enumerator = generator.GenerateStepByStep().GetEnumerator();
+	while (visitedCells < generator.Maze.Cells.Length)
+	{
+		enumerator.MoveNext();
+		iterations++;
+	}
+
+	generator.VisitedCells.MarkChanged -= OnMarkChanged;
+
+	return iterations;
+}
+
+foreach (var mazeFunc in mazeTypes)
+{
+	var testMaze = mazeFunc();
+	int edgesCount = testMaze.Cells.Sum(c => c.Neighbors.Length);
+	using StreamWriter sw = new($"{testMaze}.csv");
+
+	sw.WriteLine("deltaFactor; iterations; dead ends ratio; path length to cells ratio");
+	string[,] results = new string[tests, deltaFactors.Length];
+	int[] localSeeds = new int[tests];
+	for (int i = 0; i < tests; i++)
+	{
+		localSeeds[i] = rnd.Next();
+	}
+	Parallel.For(0, tests, i =>
+	{
+		int localSeed = localSeeds[i];
+
+		for (int j = 0; j < deltaFactors.Length; j++)
+		{
+			var maze = mazeFunc();
+			MazePath path = new(maze, maze.GetOuterWalls().First(), maze.GetOuterWalls().Last());
+			maze.Paths.Add(path);
+
+			var function = HeatMapDecayFunctions.Multiplicative(deltaFactors[j]);
+			OriginShiftParams @params = new()
+			{
+				MaxIterations = -1,
+				NeighborSelector = new HeatMapNeighborSelector(function)
+			};
+			OriginShiftGeneration generator = new(maze, localSeed, @params);
+
+			int iterations = TestGenerator(generator);
+			float deadEndsRatio = (float)maze.FindDeadEnds().Count() / maze.Cells.Length;
+			float pathLengthToCells = (float)path.GetSegments(false).Count() / maze.Cells.Length;
+			results[i, j] = $"{deltaFactors[j]}; {iterations}; {deadEndsRatio}; {pathLengthToCells}";
+		}
+	});
+	for (int i = 0; i < deltaFactors.Length; i++)
+	{
+		for (int j = 0; j < tests; j++)
+		{
+			sw.WriteLine(results[j, i]);
+		}
+	}
+}
