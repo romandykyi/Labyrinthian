2fdfae13050058478a3831c2914e44025c10717c
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-08-04 02:01:42 +0200

    Add export to file extensions

---
+++ Labyrinthian.Svg/Extensions/MazeSvgExporterExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Labyrinthian.Svg
+{
+    public static class MazeSvgExporterExtensions
+    {
+        /// <summary>
+        /// Exports a maze to a file.
+        /// </summary>
+        /// <param name="exporter">The exporter to use.</param>
+        /// <param name="filename">Filename to where the maze will be exported.</param>
+        public static void ExportToFile(this MazeSvgExporter exporter, string filename)
+        {
+            using StreamWriter streamWriter = new StreamWriter(filename);
+            using SvgWriter svgWriter = new SvgWriter(streamWriter);
+            exporter.Export(svgWriter);
+        }
+
+        /// <summary>
+        /// Exports a maze to a file asynchronously.
+        /// </summary>
+        /// <param name="exporter">The exporter to use.</param>
+        /// <param name="filename">Filename to where the maze will be exported.</param>
+        /// <returns>
+        /// A task representing the asynchronous operation.
+        /// </returns>
+        public static async Task ExportToFileAsync(this MazeSvgExporter exporter, string filename)
+        {
+            using StreamWriter streamWriter = new StreamWriter(filename);
+            using SvgWriter svgWriter = new SvgWriter(streamWriter);
+            await exporter.ExportAsync(svgWriter);
+        }
+    }
+}
